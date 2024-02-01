520de9f0d756aa1f6c75efade21b327785bb06f6
Deepesh.cholker <deepesh.cholkar@codiant.com>
2024-02-01 18:16:21 +0530

    feature/fileFilter-size-limit-multer

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,16 +1,44 @@
-import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
-import { diskStorage } from "multer";
-import { extname } from "path";
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import {
+  UnsupportedMediaTypeException,
+  PreconditionFailedException,
+} from '@nestjs/common';
 
 export const multerConfig: MulterOptions = {
-    dest: 'C:/Users/codiant/Desktop/nest js demo/nextjs-demo/src/uploads',
-    storage: diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, 'C:/Users/codiant/Desktop/nest js demo/nextjs-demo/src/uploads')
-        },
-        filename: (req, file, cb) => {
-            const fileName = `${file.originalname.split('.')[0]}.${Date.now()}${extname(file.originalname)}`
-            cb(null, fileName)
-        }
-    })
-}
+  dest: 'C:/Users/codiant/Desktop/nest js demo/nextjs-demo/src/uploads',
+  storage: diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'C:/Users/codiant/Desktop/nest js demo/nextjs-demo/src/uploads');
+    },
+    filename: (req, file, cb) => {
+      const fileName = `${
+        file.originalname.split('.')[0]
+      }.${Date.now()}${extname(file.originalname)}`;
+      cb(null, fileName);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = ['image/jpeg', 'image/png'];
+    if (allowedMimes.includes(file.mimetype)) {
+      const maxSize = 5 * 1024 * 1024;
+      if (file.size <= maxSize) {
+        cb(null, true);
+      } else {
+        cb(new PreconditionFailedException({
+            status: false,
+            message: "File size exceed the limit (5MB)"
+        }), false);
+      }
+    } else {
+      cb(
+        new UnsupportedMediaTypeException({
+          status: false,
+          message: 'Unsupported Media Type',
+        }),
+        false,
+      );
+    }
+  },
+};
